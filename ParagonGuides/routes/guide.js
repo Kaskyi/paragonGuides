f4177b0f5e3fd23b69c5566ab4961aa5deaf6bf9
Jack Kamenskiy <kamenskiygj@live.com>
2016-11-10 22:55:42 +0200

    Connect guide router to db

--- ParagonGuides/routes/guide.js
+++ ParagonGuides/routes/guide.js
@@ -1,10 +1,22 @@
 ﻿var express = require('express');
 var router = express.Router();
+var guideModel = require('../models/guide.js');
 
 router.get('/', function (req, res) {
-    res.render('guides', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page' });
+    res.render('guides');
+});
+router.get('/create', function (req, res) {
+    res.render('guide-constructor', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page' });
 });
 router.get('/:id', function (req, res) {
-    res.render('guide', { title: 'AboutID', year: new Date().getFullYear(), message: 'Your application description page' });
+    //TODO Not universal !
+    guideModel(req.db);
+    guideModel.getFullGuideByID(req.params.id, function (err, row) {
+        if (!err) {
+            res.render('guide', { guide:row , guidejson: JSON.stringify(row)});
+        } else {
+            console.error('Guide %s not found', req.params.id);
+        }
+    });
 });
 module.exports = router;
